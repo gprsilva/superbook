3389d0d5663a6d98b0317926ce87cbecc633d1e9
gprsilva <gprsilva2008@gmail.com>
2025-08-27 14:33:37 -0300

    Criando a primeira view do heroes

--- heroes/views.py
+++ heroes/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Hero
 
-# Create your views here.
+def lista_herois(request):
+    herois = Hero.objects.all()  # busca todos os heróis do banco
+    return render(request, "heroes/lista_herois.html", {"herois": herois})

157458e4038f3e2d232a71c2eba17502dc4677b2
gprsilva <gprsilva2008@gmail.com>
2025-08-27 14:47:46 -0300

    Trabalhando com FBV e CBV para os herois

--- heroes/urls.py
+++ heroes/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import HeroListView
 
 urlpatterns = [
-    path('lista/',views.lista_herois, name="lista_herois")
+    path('cbv-lista/', HeroListView.as_view(), name='cbv_lista_herois'),
 ]
--- heroes/views.py
+++ heroes/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Hero
 
-def lista_herois(request):
-    herois = Hero.objects.all()  # busca todos os heróis do banco
-    return render(request, "heroes/lista_herois.html", {"herois": herois})
+# def lista_herois(request):
+#     herois = Hero.objects.all()  # busca todos os heróis do banco
+#     return render(request, "heroes/lista_herois.html", {"herois": herois})
+
+class HeroListView(ListView):
+    model = Hero
+    template_name = "heroes/lista_herois.html"
+    context_object_name = "herois"
